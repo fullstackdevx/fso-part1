914f53abc77da77aef8b0cef7cef3ef778c3e1c6
“fsdx” <fullstackdevx@gmail.com>
2021-02-10 10:52:01 +0100

    courseinfo step4

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -5,7 +5,8 @@ const Header = ({ course }) => {
   return (<h1>{course}</h1>)
 }
 
-const Content = ({ part1, part2, part3 }) => {
+const Content = ({ parts }) => {
+  const [part1, part2, part3] = parts
   return (
     <div>
       <Part name={part1.name} exercises={part1.exercises} />
@@ -19,30 +20,33 @@ const Part = ({ name, exercises }) => {
   return (<p>{name} {exercises}</p>)
 }
 
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return (<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>)
+const Total = ({ parts }) => {
+  const total = parts.reduce((accum, part) => accum + part.exercises, 0)
+  return (<p>Number of exercises {total}</p>)
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
