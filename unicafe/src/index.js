7501f39026df6039f88a512fe238979a9476d6ed
“fsdx” <fullstackdevx@gmail.com>
2021-02-10 12:46:35 +0100

    unicafe step2

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const GOOD_AVERAGE_WEIGHT = 1
+const NEUTRAL_AVERAGE_WEIGHT = 0
+const BAD_AVERAGE_WEIGHT = -1
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -10,6 +14,10 @@ const App = () => {
   const increaseGoodByOne = () => setGood(good + 1)
   const increaseNeutralByOne = () => setNeutral(neutral + 1)
   const increaseBadByOne = () => setBad(bad + 1)
+
+  const all = () => good + neutral + bad
+  const averageScore = () => all() === 0 ? 0 : (good * GOOD_AVERAGE_WEIGHT + neutral * NEUTRAL_AVERAGE_WEIGHT + bad * BAD_AVERAGE_WEIGHT) / all()
+  const positivePercentage = () => all() === 0 ? 0 : good * 100 / all()
 
   return (
     <div>
@@ -21,6 +29,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all()}</p>
+      <p>average {averageScore()}</p>
+      <p>positive {positivePercentage()} %</p>
     </div>
   )
 }
