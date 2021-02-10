9772dad74e5f10d3f0c2509a7376d9052b6d64af
“fsdx” <fullstackdevx@gmail.com>
2021-02-10 11:52:56 +0100

    unicafe step1

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -7,9 +7,20 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const increaseGoodByOne = () => setGood(good + 1)
+  const increaseNeutralByOne = () => setNeutral(neutral + 1)
+  const increaseBadByOne = () => setBad(bad + 1)
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={increaseGoodByOne}>good</button>
+      <button onClick={increaseNeutralByOne}>neutral</button>
+      <button onClick={increaseBadByOne}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
